241188cc7b221b2c8588d0332267a56fc171f7c9
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-09-16 17:23:19 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,9 +1,10 @@
 require 'pry'
 
 def square_array(array)
-  numbers = [1, 2, 3]
+  array = [1, 2, 3]
   squares = []
-  numbers.each do |number|
+  array.each do |number|
     squares << (number ** 2)
   end
+  squares
 end
